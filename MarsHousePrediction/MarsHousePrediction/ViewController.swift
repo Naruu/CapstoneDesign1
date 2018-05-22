b54b5d50389554edbec053ca7b713cda932d3153
fredhur <fredhur36@gmail.com>
2018-05-23 08:57:14 +0900

    class work done

--- MarsHousePrediction/MarsHousePrediction/ViewController.swift
+++ MarsHousePrediction/MarsHousePrediction/ViewController.swift
@@ -14,7 +14,7 @@ enum Feature : Int {
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    let pickerDataSource = PickerDataSource()
+    let pickerDataSource = self
     
     @IBOutlet weak var pickerView: UIPickerView! {
         didSet{
@@ -30,6 +30,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
+    
+    private let solarPanelsDataSource = SolarPanelDataSource()
+    private let greenhousesDataSource = GreenhousesDataSource()
+    private let sizeDataSource = SizeDataSource()
+    
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int{
         return 3
     }
@@ -39,11 +45,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         case .solarPanels:  return solarPanelsDataSource.values.count
         case .greenhouses:  return greenhousesDataSource.values.count
         case .size:         return sizeDataSource.values.count
-            
         }
     }
     
- ≈
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         guard let feature = Feature(rawValue: component) else{
@@ -55,8 +59,26 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print("Selected row = ", row)
     }
+
     
+    func title(for row: Int, feature: Feature) -> String?{
+        switch feature{
+        case .solarPanels:  return solarPanelsDataSource.title(for: row)
+        case .greenhouses:  return greenhousesDataSource.title(for: row)
+        case .size:         return sizeDataSource.title(for: row)
+        }
+    }
     
+    func value(for row: Int, feature: Feature) -> Double{
+        let value: Double?
+        
+        switch feature{
+        case .solarPanels: value = solarPanelsDataSource.value(for: row)
+        case .greenhouses: value = greenhousesDataSource.value(for: row)
+        case .size:        value = sizeDataSource.value(for: row)
+        }
+        return value!
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -66,7 +88,54 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+struct SolarPanelDataSource {
+    
+    let values = [1,1.5,2,2.5,3,3.5,4,4.5,5]
+    
+    func title(for index: Int) -> String?{
+        guard index < values.count else { return nil }
+        return String(values[index])
+    }
+    
+    func value(for index: Int) ->Double? {
+        guard index < values.count else { return nil}
+        return Double(values[index])
+    }
+}
+
+struct GreenhousesDataSource {
+    let values = [1,2,3,4,5]
+    
+    func title(for index: Int) -> String?{
+        guard index < values.count else { return nil }
+        return String(values[index])
+    }
+    
+    func value(for index: Int) ->Double? {
+        guard index < values.count else { return nil}
+        return Double(values[index])
+    }
+}
+
+struct SizeDataSource {
+    private static let numberFormatter : NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.locale = .current
+        formatter.numberStyle = .decimal
+        formatter.usesGroupingSeparator = true
+        return formatter
+    }()
+    
+    let values = [750, 1000, 1500, 2000, 3000, 4000, 5000, 10_000]
+    
+    func title(for index: Int) -> String?{
+        guard index < values.count else { return nil }
+        return String(values[index])
+    }
+    func value(for index: Int) ->Double? {
+        guard index < values.count else { return nil}
+        return Double(values[index])
+    }
+}
